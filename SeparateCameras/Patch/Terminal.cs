421cb9c690144851f9e2b80788e84a6f570867ee
cptkumquat <154663176+cptkumquat@users.noreply.github.com>
2023-12-23 23:23:29 -0800

    add override for `switch <playerName>`

--- SeparateCameras/Patch/Terminal.cs
+++ SeparateCameras/Patch/Terminal.cs
@@ -32,6 +32,9 @@ namespace SeparateCameras.Patch
     {
         static ManualCameraRenderer mcr;
 
+        /**
+         * Override `switch` to only change terminal radar
+         */
         [HarmonyPatch(nameof(Terminal.RunTerminalEvents))]
         [HarmonyPrefix]
         static void RunTerminalEvents_Before(Terminal __instance, TerminalNode __0)
@@ -88,6 +91,124 @@ namespace SeparateCameras.Patch
             }
 
             Plugin.LogInfo("RunTerminalEvents: EXIT");
+        }
+
+        /**
+         * Override `switch <playerName>` to only change terminal radar
+         */
+        [HarmonyPatch("ParsePlayerSentence")]
+        [HarmonyPrefix]
+        static bool ParsePlayerSentence_Before(Terminal __instance, ref TerminalNode __result, ref bool ___broadcastedCodeThisFrame)
+        {
+            ___broadcastedCodeThisFrame = false;
+            string s = __instance.screenText.text.Substring(__instance.screenText.text.Length - __instance.textAdded);
+            s = RemovePunctuation(s);
+            string[] array = s.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+            if (__instance.currentNode != null && __instance.currentNode.overrideOptions)
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    TerminalNode terminalNode = ParseWordOverrideOptions(array[i], __instance.currentNode.terminalOptions);
+                    if (terminalNode != null)
+                    {
+                        __result = terminalNode;
+                        return false;
+                    }
+                }
+                __result = null;
+                return false;
+            }
+
+            if (array.Length > 1 && array[0] == "switch")
+            {
+                int num = CheckForPlayerNameCommand(array[0], array[1]);
+                if (num != -1)
+                {
+                    mcr.SwitchRadarTargetAndSync(num);
+                    __result = __instance.terminalNodes.specialNodes[20];
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private static string RemovePunctuation(string s)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (char c in s)
+            {
+                if (!char.IsPunctuation(c))
+                {
+                    stringBuilder.Append(c);
+                }
+            }
+
+            return stringBuilder.ToString().ToLower();
+        }
+
+        private static TerminalNode ParseWordOverrideOptions(string playerWord, CompatibleNoun[] options)
+        {
+            for (int i = 0; i < options.Length; i++)
+            {
+                for (int num = playerWord.Length; num > 0; num--)
+                {
+                    if (options[i].noun.word.StartsWith(playerWord.Substring(0, num)))
+                    {
+                        return options[i].result;
+                    }
+                }
+            }
+
+            return null;
+        }
+        private static int CheckForPlayerNameCommand(string firstWord, string secondWord)
+        {
+            if (firstWord == "radar")
+            {
+                return -1;
+            }
+
+            if (secondWord.Length <= 2)
+            {
+                return -1;
+            }
+
+            Debug.Log("first word: " + firstWord + "; second word: " + secondWord);
+            List<string> list = new List<string>();
+            for (int i = 0; i < mcr.radarTargets.Count; i++)
+            {
+                list.Add(mcr.radarTargets[i].name);
+                Debug.Log($"name {i}: {list[i]}");
+            }
+
+            secondWord = secondWord.ToLower();
+            for (int j = 0; j < list.Count; j++)
+            {
+                string text = list[j].ToLower();
+                if (text == secondWord)
+                {
+                    return j;
+                }
+            }
+
+            Debug.Log($"Target names length: {list.Count}");
+            for (int k = 0; k < list.Count; k++)
+            {
+                Debug.Log("A");
+                string text = list[k].ToLower();
+                Debug.Log($"Word #{k}: {text}; length: {text.Length}");
+                for (int num = secondWord.Length; num > 2; num--)
+                {
+                    Debug.Log($"c: {num}");
+                    Debug.Log(secondWord.Substring(0, num));
+                    if (text.StartsWith(secondWord.Substring(0, num)))
+                    {
+                        return k;
+                    }
+                }
+            }
+
+            return -1;
         }
     }
 
